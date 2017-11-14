94d83cf4194d59740f02063ba434a6047042ae77
EdgarAlenPoe <eganschafer@gmail.com>
2017-11-14 10:03:10 -0700

    Added Gui, bug fixes.
    Added a gui system using JOptionPane and JFileChooser.
    Also removed major bugs with splitting on line breaks.

--- Shuffle/src/Shuffle.java
+++ Shuffle/src/Shuffle.java
@@ -1,25 +1,66 @@
 import java.util.List;
+import java.awt.HeadlessException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
-
+import java.io.PrintStream;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Shuffle {
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
-	public static void main(String[] args) {
+public class Shuffle { //TODO fix linebreak handling. words are being scrambled across linebreaks with changing length.
+
+	public static void main(String[] args) throws HeadlessException, IOException {
 		Shuffle thing = new Shuffle();
-		System.out.println(thing.shuffleInput(System.in));
+//		System.out.println(thing.shuffleWord("laws\nof"));
+		JOptionPane.showMessageDialog(null, "This program will take a text file input, shuffle all words, then output into a new file in the same directory.");
+		JFileChooser chooser = new JFileChooser();
+		chooser.showOpenDialog(null);
+		File file = chooser.getSelectedFile();
+		
+		File outFile = new File(file.getParent() + File.separator + JOptionPane.showInputDialog("Enter name for output file."));
+		
+		if (!outFile.createNewFile()) {
+			JOptionPane.showConfirmDialog(null, "File already exists. Overwrite it?");
+		}
+		
+		PrintStream output = new PrintStream(outFile);
+		
+		
+		
+		try {
+			output.println(thing.shuffleInput(new FileInputStream(file)));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		finally {
+			output.close();
+		}
+		
 	}
+	
+	private Pattern pattern = Pattern.compile(".*([- .,';:!?\\n\\r]).*");
 
 	public String shuffleInput(InputStream input) {
 		Scanner scan = new Scanner(input);
 		scan.useDelimiter(" ");
 		StringBuilder output = new StringBuilder();
 		String next;
+		int wordNum = 0;
 		while (scan.hasNext()) {
 			next = scan.next();
-			output.append(shuffleWord(next) + " ");
+//			System.err.println(wordNum);
+			wordNum++;
+			String nextOut = shuffleWord(next);
+			output.append(nextOut + " ");
 		}
 		scan.close();
 		return output.toString();
@@ -41,19 +82,44 @@ public class Shuffle {
 		Random rand = new Random();
 		List<Character> set = new LinkedList<Character>();
 		List<Integer> literals = new LinkedList<Integer>();
-		if (s.length() < 2) {
+		Matcher match = pattern.matcher(s);
+		
+		if (s.length() < 3) {
+//			System.err.println("debug, skipping");
 			return s;
 		}
-		else if (s.substring(0, 1).matches("[- .,';:!?\\\n]")) {
-			return s.substring(0,1) + shuffleWord(s.substring(1));
+		else if (match.matches()) {
+			
+			
+//			System.err.println("debug, splitting");
+			int splitPoint = s.indexOf(match.group(1));
+//			System.err.println("splitPoint: " + match.group(1));
+//			System.err.println("getting first part");
+			String firstPart = shuffleWord(s.substring(0, splitPoint));
+//			System.err.println("getting last part");
+//			System.err.println(s.substring(splitPoint + 1));
+			String lastPart = shuffleWord(s.substring(splitPoint + 1));
+			return  firstPart + match.group(1) + lastPart;
 		}
-		else if (s.substring(s.length()-1).matches("[- .,';:!?\\\n]")) {
-			return shuffleWord(s.substring(0,s.length()-1)) + s.substring(s.length()-1);
-		}
+//		else if (s.substring(0, 1).matches("[- .,';:!?\\n\\r]")) {
+//			System.err.println("debug, splitting");
+//			return s.substring(0,1) + shuffleWord(s.substring(1));
+//		}
+//		else if (s.substring(s.length()-1).matches("[- .,';:!?\\n\\r]")) {
+//			System.err.println("debug, splitting");
+//			return shuffleWord(s.substring(0,s.length()-1)) + s.substring(s.length()-1);
+//		}
 		else {
+//			System.err.println("debug, scrabbling");
 			for (int x = 1; x < (s.length() - 1);x++) {
-				if (!String.valueOf(s.charAt(x)).matches("[- .,';:!?\\\n]"))set.add(s.charAt(x));
-				else literals.add(x);
+				if (!String.valueOf(s.charAt(x)).matches("[- .,';:!?\\n\\r]")) {
+//					System.err.println("Inserting scramble");
+					set.add(s.charAt(x));
+				}
+				else  {
+//					System.err.println("Inserting literal");
+					literals.add(x);
+				}
 			}
 			
 			StringBuilder newString = new StringBuilder(s);
